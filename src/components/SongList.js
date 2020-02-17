4ca36b5ce436da91dec6294a27c0346705a6b8e2
Arvin Lleva <arvin.lleva@yahoo.com>
2020-02-16 17:45:09 -0800

    Implemented the use for the UseEffect hook for understanding how it works

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react' // useEffect gies us access to lifecycle methods
 import uuid from 'uuid'
 import NewSongFrom from './NewSongForm'
 
@@ -9,11 +9,28 @@ const SongList = () => {
     { title: 'this wild darkness', id: 3}
   ])
 
+  const [age, setAge] = useState(0)
+
   const addSong = (song) => {
     setSongs([...songs, {
       title: song,
       id: uuid.v4() // this generates a random id
     }])
+  }
+
+  useEffect(() => {
+    console.log("Initial ender", songs, age)
+  }, [])
+
+  useEffect(() => {
+    console.log("Current songs", songs)
+  }, [songs])
+  useEffect(() => {
+    console.log("Current age", age)
+  }, [age])
+
+  const addAge = () => {
+    setAge(age + 1)
   }
 
   const displaySongs = songs.map(song => {
@@ -30,6 +47,7 @@ const SongList = () => {
        {displaySongs} 
       </ul>
       <NewSongFrom addSong={addSong}/>
+      <button onClick={addAge}>Add 1 to age: {age}</button>
     </div>
   )
 }
