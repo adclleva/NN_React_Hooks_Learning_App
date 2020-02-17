a471d6cffc2fe7e186cac422055fe70cd0111862
Arvin Lleva <arvin.lleva@yahoo.com>
2020-02-16 16:50:23 -0800

    utilized the useState hook to display songs and also added songs with the uuid library

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import uuid from 'uuid'
 
 const SongList = () => {
+  const [songs, setSongs] = useState([
+    { title: 'almost home', id: 1},
+    { title: 'memry gospel', id: 2},
+    { title: 'this wild darkness', id: 3}
+  ])
+
+  const addSong = () => {
+    setSongs([...songs, {
+      title: 'new song',
+      id: uuid.v4() // this generates a random id
+    }])
+  }
+
+  const displaySongs = songs.map(song => {
+    const { title, id } = song
+
+    return(
+    <li key={id}>{title}</li>
+    )
+  })
+
   return (
     <div className="song-list">
       <ul>
-        <li>this wild darkeness</li>
-        <li>memory gospel</li>
+       {displaySongs} 
       </ul>
+      <button onClick={() => addSong()}>Add Song</button>
     </div>
   )
 }
